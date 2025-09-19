48c2904de07e3b5215b0a9086c15c739bdf8e162
mahmudsajeeb <saifurrahmansajib37@gmail.com>
2025-09-19 14:28:16 +0600

    n*2 time complexity

--- O(N)/on.py
+++ O(N)/on.py
@@ -1,12 +1,21 @@
-def find_name(names, target):
-    for name in names:    
-        if name == target:
-            return f"{target}  "
-    return f"{target}    "
+# def find_name(names, target):
+#     for name in names:    
+#         if name == target:
+#             return f"{target}  "
+#     return f"{target}    "
 
 
  
-students = ["Sajib", "Rahim", "Karim", "Jamil"]
+# students = ["Sajib", "Rahim", "Karim", "Jamil"]
 
-print(find_name(students, "Rahim"))    
-print(find_name(students, "Hasan"))    
+# print(find_name(students, "Rahim"))    
+# print(find_name(students, "Hasan"))    
+def find_name(names,target):
+    for name in names:
+        if name == target:
+            return f"{target}  find Name"
+    else:
+        return f"{target} Did Not find Name"
+students = ["Sajib", "Rahim", "Karim","Jamil"]
+print(find_name(students,"Rahim"))
+print (find_name(students,"Munmun"))
---
+++ on2/on2.py
@@ -0,0 +1,7 @@
+n = int (input("Enter: "))
+nn = int(input("Enter: "))
+nnn = int(input("Enter: "))
+
+for i in range(1, n+1):
+    for j in range(1, j+1):
+        print("Hello")
